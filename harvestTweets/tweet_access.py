c6620450d8dcf4037f37e0b3876efea96406b4ca
Rebecca Stevens <rsbjshan@gmail.com>
2017-11-20 21:28:57 +0000

    Update tweet_access.py

--- harvestTweets/tweet_access.py
+++ harvestTweets/tweet_access.py
@@ -5,10 +5,10 @@ from collections import Counter
 from prettytable import PrettyTable
 
 # Replace these values with our own twitter app settings
-CONSUMER_KEY = 'MT6KMuqBegbfAxU8ZeGBxmIG5'
-CONSUMER_SECRET = 'VdRlAQxTqM1oWNDuW9etzWzmZIUwgJRA02REDyzzmRHYSYu72z'
-OAUTH_TOKEN = '3700798289-PBuFNcTWQwNBVAmk2IghTYPnLosmPxbCshLOE3n'
-OAUTH_TOKEN_SECRET = 'oQJrDYAHYOA4PqBia6a0uWHBikmwZfb2a975FpDYDrscf'
+CONSUMER_KEY = '
+CONSUMER_SECRET = ''
+OAUTH_TOKEN = ''
+OAUTH_TOKEN_SECRET = ''
 
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
